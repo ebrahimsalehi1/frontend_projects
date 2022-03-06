45bb30722147a5a5c7c034b96ae10829e921e19a
eb.salehi <eeeee.bbbbb.iiiii@gmail.com>
2022-03-06 09:29:05 +0330

    03-06-2022

--- Python/JamesAndEconomicProblems.py
+++ Python/JamesAndEconomicProblems.py
@@ -6,20 +6,26 @@ n , k = int(line[0]) , int(line[1])
 arr = list(map(lambda x:int(x),input('').strip().split(' ')))
 arr = sorted(arr)
 
-i=0
-j=n-1
-while True:
-    if i>=j:
-        break
+s = sum(arr)
+all = n-(s//k)-(1 if s%k!=0 else 0)
+if all<0:
+    all=0
 
-    if arr[j]<k and arr[j]>0:
-        arr[i]=arr[i]-(k-arr[j])
-        arr[j]=k
-        j=j-1
-        if arr[i]==0:
-            i=i+1
-    else:
-        j=j-1
+print(all)
+# i=0
+# j=n-1
+# while True:
+#     if i>=j:
+#         break
 
-cnt = sum(map(lambda x:x==0,arr))
-print(cnt)
+#     if arr[j]<k and arr[j]>0:
+#         arr[i]=arr[i]-(k-arr[j])
+#         arr[j]=k
+#         j=j-1
+#         if arr[i]==0:
+#             i=i+1
+#     else:
+#         j=j-1
+
+# cnt = sum(map(lambda x:x==0,arr))
+# print(cnt)

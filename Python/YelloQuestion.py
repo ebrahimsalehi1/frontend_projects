025efccbdfc9644c716698f0b30e76716fef7a43
ebrahim salehi <eeeee.bbbbb.iiiii@gmail.com>
2022-03-11 12:37:53 +0330

    2022-03-08

--- Python/YelloQuestion.py
+++ Python/YelloQuestion.py
@@ -1,11 +1,28 @@
-import sys
+# My Information
+# ---------------------------------------
+# import sys
 
-n = int(input(''))
-if not(n>=1 and n<=10):
-    sys.exit()
+# n = int(input(''))
+# if not(n>=1 and n<=10):
+#     sys.exit()
 
-s=''
-for i in range(n):
-    s+='o'
+# s=''
+# for i in range(n):
+#     s+='o'
     
-print('W{}w!'.format(s))
+# print('W{}w!'.format(s))
+
+# ---------------------------------------
+
+import os 
+os.system("cls")
+
+try:
+    number = int(input("please enter number of 'O' Charachter: "))
+    if number <1 or number> 10:
+        print("enter between 1 and 10")
+    else:
+        print('W' + (number * 'o')+'w!')
+
+except ValueError :
+    print("please enter just number ")
